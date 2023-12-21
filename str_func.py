efffd0973d5104d575debd3dffaf07b1d2d3de0e
alenaodinets <alena.odinecz2013@yandex.ru>
2023-12-21 21:59:08 +0400

    new_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_words(line):
     """Возвращает строку в верхнем регистре"""
     return line.upper()
+
+def title_words(line):
+    """Возвращает строки с первыми заглавными буквами"""
+    return line.title()
+
